68fe4fd5108a48d8cde6310c9f3945bc7be2f360
avidvesha <apit.scorpio10@gmail.com>
2024-11-11 17:27:40 +0700

    data types

---
+++ data_type.php
@@ -0,0 +1,9 @@
+<?php
+$angka = 28; // Integer
+$desimal = 5.75; // Float
+$teks = "Coding is fun!"; // String
+
+// Nge-print hasilnya
+echo "Angka: $angka <br>";
+echo "Desimal: $desimal <br>";
+echo "Teks: $teks";

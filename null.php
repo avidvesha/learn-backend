9b17273e7a0ba8794b204c2b95c1faba22287adb
avidvesha <apit.scorpio10@gmail.com>
2024-11-11 17:27:54 +0700

    null

---
+++ null.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Avidvesha"; // Variabel $nama belum diisi nilainya
+if (is_null($nama)) { // Kita cek apakah variabel $nama null
+    echo "Nama belum diisi!";
+} else {
+    echo "Halo, nama saya $nama.";
+}

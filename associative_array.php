9a1d3d0b1f2e5f4231b4db5c278b8abef78b89b9
avidvesha <apit.scorpio10@gmail.com>
2024-11-11 17:28:51 +0700

    associative array

---
+++ associative_array.php
@@ -0,0 +1,11 @@
+<?php
+$siswa = array(
+    "nama" => "Budi",
+    "umur" => 17,
+    "nilai" => 85.5,
+);
+
+// Mengakses nilai array asosiatif
+echo "Nama siswa: " . $siswa["nama"] . "<br>";
+echo "Umur siswa: " . $siswa["umur"] . " tahun<br>";
+echo "Nilai siswa: " . $siswa["nilai"];

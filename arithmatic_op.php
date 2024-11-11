514c5bb46bf5e0e96d48bac8083ba6fe655ac34a
avidvesha <apit.scorpio10@gmail.com>
2024-11-11 17:28:05 +0700

    arithmatic operator

---
+++ arithmatic_op.php
@@ -0,0 +1,12 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$jumlah = $angka1 + $angka2; // Penjumlahan
+$kurang = $angka1 - $angka2; // Pengurangan
+$kali = $angka1 * $angka2; // Perkalian
+$bagi = $angka1 / $angka2; // Pembagian
+
+echo "Hasil penjumlahan: $jumlah <br>";
+echo "Hasil pengurangan: $kurang <br>";
+echo "Hasil perkalian: $kali <br>";
+echo "Hasil pembagian: $bagi <br>";

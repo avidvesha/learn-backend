41654c084729f912bdc24e8001c0197b0514215e
avidvesha <apit.scorpio10@gmail.com>
2024-11-11 22:14:24 +0700

    switch statement

---
+++ switch_statement.php
@@ -0,0 +1,30 @@
+<?php
+echo "--- SWITCH STATEMENT ---";
+echo "<br>";
+
+$hari = "Juma";
+switch ($hari) {
+    case "Senin":
+        echo "Hari ini adalah Senin.";
+        break;
+    case "Selasa":
+        echo "Hari ini adalah Selasa.";
+        break;
+    case "Rabu":
+        echo "Hari ini adalah Rabu.";
+        break;
+    case "Kamis":
+        echo "Hari ini adalah Kamis.";
+        break;
+    case "Jumat":
+        echo "Hari ini adalah Jumat.";
+        break;
+    case "Sabtu":
+        echo "Hari ini adalah Sabtu.";
+        break;
+    case "Minggu":
+        echo "Hari ini adalah Minggu.";
+        break;
+    default:
+        echo "Hari tidak valid.";
+}
